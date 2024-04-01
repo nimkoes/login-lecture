8a122f9fce7da8ac8a704ba73796e18323b6360e
nimkoes <xxxelppa@gmail.com>
2024-04-01 23:03:08 +0900

    create front-end javascript

--- app/app.js
+++ app/app.js
@@ -26,6 +26,7 @@ const home = require("./src/routes/home")
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
+app.use(express.static(`${__dirname}/src/public`)); // ${__dirname} : 현재 app.js 파일이 있는 위치 반환
 app.use("/", home);  // use -> 미들 웨어를 등록해주는 메서드
 
 module.exports = app;
