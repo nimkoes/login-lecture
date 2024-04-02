068eca68e356f41e62968288233f01677705a6dd
seokmin <smhong@tradlinx.com>
2024-04-03 08:48:57 +0900

    BE 로그인 기능 추가

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,5 +1,6 @@
 "use strict";
 
+
 const output = {
     home: (req, res) => {
         res.render("home/index");
@@ -9,9 +10,29 @@ const output = {
     }
 }
 
+const users = {
+    id: ["nimkoes", "admin", "potato"],
+    psword: ["1234", "1234", "123456"]
+};
+
 const process = {
     login: (req, res) => {
-        console.log(req.body);
+        const id = req.body.id,
+          psword = req.body.psword;
+
+        if(users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if(users.psword[idx] === psword) {
+                return res.json({
+                    success: true
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하셨습니다."
+        })
     }
 }
 
